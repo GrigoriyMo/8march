ed235cf6d1b84cc5f509d2d56b39e5365ef5bfd2
GrigoriyMo <kovarscky.g@yandex.ru>
2023-03-05 00:41:43 +0300

    first

---
+++ src/components/HelloPage.vue
@@ -0,0 +1,74 @@
+
+<template>
+  <h1></h1>
+  <ul class="partner-list">
+    <li v-for="item in partners" :key="item.id">
+      <partnerCard
+        :img="item.img"
+        :title="item.name"
+        :description="item.description"
+      ></partnerCard>
+    </li>
+  </ul>
+</template>
+<script>
+import partnerCard from "./Partner.vue";
+export default {
+  name: "Main",
+  components: {
+    partnerCard: partnerCard,
+  },
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      partners: [
+        {
+          id: 1,
+          name: `Таблетки для медитации "Натали+"`,
+          img: `public/ladybug.png`,
+          description: `Бесплатная доставка по подписке`,
+        },
+        {
+          id: 2,
+          name: `Канарские Уралы`,
+          img: `public/ladybug.png`,
+          description: `Ветаптека и пиво для собак со скидкой 50%`,
+        },
+        {
+          id: 3,
+          name: `Корейский Эчпочмакико`,
+          img: `public/ladybug.png`,
+          description: `Кэшбэк чак-чаком на 25% больше`,
+        },
+        {
+          id: 4,
+          name: `Ленинградский пассаж "Хоббит-СПБ"`,
+          img: `public/ladybug.png`,
+          description: `7 гномов в подарок по подписке при покупке штанов Гендальфа`,
+        },
+        {
+          id: 5,
+          name: `Группа компаний "Новичок"`,
+          img: `public/ladybug.png`,
+          description: `Заведем новичка по подписке, заберем  долги`,
+        },
+      ],
+      partnersCount: 5,
+    };
+  },
+};
+</script>
+<style scoped>
+
+.partner-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+li {
+    list-style-type: none;
+   }
+
+</style>
---
+++ src/components/Partner.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="partner-card-wrapper">
+        <p class="partner-card-title"> <b>{{ title }}</b></p>
+        <img class="partner-card-img" :src="img" alt="">
+        <p class="partner-card-p"> {{ description }}</p>
+    </div>
+</template>
+<script>
+export default {
+  name: "PartnerCard",
+  props:{
+    img:String,
+    title:String,
+    description:String
+  },
+  data(){
+    return {
+
+    }
+  }
+};
+</script>
+<style scoped>
+.partner-card-wrapper{
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: space-evenly;
+    width:250px;
+    height: 400px;
+    background-color:#483AF0;
+    border-radius: 30px;
+    border:10px solid #753FD9;
+    margin:10px;
+}
+
+.partner-card-wrapper:hover{
+    cursor: pointer;
+    transform: scale(1.1);
+    border: 10px solid #483AF0;
+}
+
+.partner-card-img{
+    width: 250px;
+    height: 250px;
+    margin: 0 auto;
+}
+</style>
